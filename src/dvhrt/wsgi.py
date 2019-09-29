5d842ffa64703ff85387786daff552a76df35fa7
Barry Eom <31739208+barieom@users.noreply.github.com>
2019-09-29 15:51:45 -0400

    gitignore update

--- src/dvhrt/wsgi.py
+++ src/dvhrt/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutvwebsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvhrtwebsite.settings')
 
 application = get_wsgi_application()
