b391f93f24855077763920213b0e1bd4b0442d10
Nicole <nlkennedy7@gmail.com>
2019-10-08 21:54:54 -0400

    person and community classes

--- src/api/models.py
+++ src/api/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+class Community(models.Model):
+    community_id = models.IntegerField()
+    referral_sources = models.ArrayField(models.CharField())
+
+class Person(models.Model):
+    gender_choices = (
+        (0, 'undefined'),
+        (1, 'Female'),
+        (2, 'Male'),
+        (3, 'Other'),
+    )
+
+    race_ethnicity_choices = (
+        (0, 'undefined'),
+        (1, 'American Indian/Alaska Native'),
+        (2, 'Asian'),
+        (3, 'Black/African American'),
+        (4, 'Hispanic or Latino'),
+        (5, 'Native Hawaiian/Pacific Islander'),
+        (6, 'White'),
+        (7, 'Other/Unknown'),
+    )
+
+    age_at_case_acceptance_choices = (
+        (0, 'undefined'),
+        (1, '13 or younger'),
+        (2, '14-17'),
+        (3, '18-19'),
+        (4, '20-29'),
+        (5, '30-39'),
+        (6, '40-49'),
+        (7, '50-59'),
+        (8, '60+'),
+        (9, 'Unknown'),
+    )
+
+    person_id = models.IntegerField()
+    is_victim = BooleanField()
+    name = models.CharField()
+    dob = DateField()
+    gender = EnumField(choices=gender_choices, default=0)
+    race_ethnicity = EnumField(choices=race_ethnicity_choices, default=0)
+    age_at_case_acceptance = EnumField(choices=age_at_case_acceptance_choices, default=0)
+    primary_language = models.CharField()
+    town = models.CharField()
