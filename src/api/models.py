8d36c6840829ab98618df8a2fc2b8515ca2b00be
khaliunn <khaliun.narangerel@tufts.edu>
2019-10-16 22:16:13 -0400

    Made indices zero-based

--- src/api/models.py
+++ src/api/models.py
@@ -4,21 +4,21 @@ from django.db import models
 class Case(models.Model):
 
 	RELATIONSHIP_TYPE = [
-		(1, 'Current Spouse/Intimate Partner'),
-		(2, 'Former Spouse/Intimate Partner'),
-		(3, 'Current Dating Relationship'),
-		(4, 'Former Dating Relationship'),
-		(5, 'Other'),
+		(0, 'Current Spouse/Intimate Partner'),
+		(1, 'Former Spouse/Intimate Partner'),
+		(2, 'Current Dating Relationship'),
+		(3, 'Former Dating Relationship'),
+		(4, 'Other'),
 	]
 
 	RELATIONSHIP_LENGTH = [
-		(1, '<1 year'),
-		(2, '1-5 years'),
-		(3, '6-9 years'),
-		(4, '10-14 years'),
-		(5, '15-19 years'),
-		(6, '20-29 years'),
-		(7, '30+ years'),
+		(0, '<1 year'),
+		(1, '1-5 years'),
+		(2, '6-9 years'),
+		(3, '10-14 years'),
+		(4, '15-19 years'),
+		(5, '20-29 years'),
+		(6, '30+ years'),
 	]
 
 	case_id = models.IntegerField(primary_key=True)
