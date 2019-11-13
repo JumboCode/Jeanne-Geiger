540161642fd892a82e567b99045a20e44ba98b11
Barry Eom <31739208+barieom@users.noreply.github.com>
2019-11-12 21:45:52 -0500

    settings and url fixed

--- src/dvhrt/urls.py
+++ src/dvhrt/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import *
 
-from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
