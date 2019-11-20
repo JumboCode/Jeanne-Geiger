4fda80c917154b5d0470d10140f5fba7806dd6de
MahBoiVladdyP <trev0407@gmail.com>
2019-11-19 22:12:51 -0500

    Added pipeframes for all routes

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -9,7 +9,7 @@ import Login from './modules/logIn';
 import addCase from './modules/comManager/addCase';
 import comOverview from './modules/comManager/comOverview';
 import detailView from './modules/comManager/detailView';
-import editCase from './modules/comManager/editCase';
+//import editCase from './modules/comManager/editCase';
 
 //import PublicRoute from 'Public';
 //import PrivateRoute from 'Private';
@@ -21,7 +21,11 @@ export default class AppRouter extends React.Component {
         <Switch>
           <Route exact path="/" component = {Login} type = "login"/>
           <Route exact path="/admin" component = {adminHomepage} type = "admin"/>
-          <Route exact path="/admin/site" component = {adminComOverview} type = "comoverview"/>
+          <Route exact path="/admin/site" component = {adminComOverview} type = "adminoverview"/>
+	  <Route exact path="/site" component = {comOverview} type = "comOverview"/>
+	  <Route exact path="/admin/site-view" component = {adminViewCaseDetail} type = "comCaseDetail"/>
+	  <Route exact path="/site/add-case" component = {addCase} type = "addCase"/>
+	  <Route exact path="/site/case-view" component = {detailView} type = "caseView"/>
         </Switch>
       </BrowserRouter>
     );
