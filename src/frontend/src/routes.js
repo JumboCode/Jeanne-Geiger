b9c1386fa839163501938ac610fc48e6db3afb56
MahBoiVladdyP <trev0407@gmail.com>
2019-11-24 18:29:03 -0500

    Created all modules and updated routes to reflect the new modules

--- src/frontend/src/routes.js
+++ src/frontend/src/routes.js
@@ -1,32 +1,32 @@
 import React from 'react'
 import { Route, BrowserRouter, Switch, Link } from 'react-router-dom'
 
-import adminComOverview from './modules/admin/adminComOverview'
-import adminHomepage from './modules/admin/adminHomepage'
-import adminViewCaseDetail from './modules/admin/adminViewCaseDetail'
 import Login from './modules/logIn'
 
-import addCase from './modules/comManager/addCase'
-import comOverview from './modules/comManager/comOverview'
-import detailView from './modules/comManager/detailView'
-// import editCase from './modules/comManager/editCase';
+import adminAddSite from './modules/admin/adminAddSite'
+import adminHomepage from './modules/admin/adminHomepage'
+import adminViewSite from './modules/admin/adminViewSite'
+
+import siteOverview from './modules/coords/siteOverview'
+import detailView from './modules/coords/detailView'
+
+import accRecovery from './modules/accRecovery'
 
 // import PublicRoute from 'Public';
 // import PrivateRoute from 'Private';
 
-/* <Route exact path="/site" component = {comOverview} type = "comOverview"/>
-     <Route exact path="/admin/site-view" component = {adminViewCaseDetail} type = "comCaseDetail"/>
-     <Route exact path="/site/add-case" component = {addCase} type = "addCase"/>
-     <Route exact path="/site/case-view" component = {detailView} type = "caseView"/>
-*/
 export default class AppRouter extends React.Component {
   render () {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component = {Login} type = "login"/>
-          <Route exact path="/admin" component = {adminHomepage} type = "admin"/>
-          <Route exact path="/admin/site" component = {adminComOverview} type = "adminoverview"/>
+          <Route exact path="/admin" component = {adminHomepage} type = "adminoverview"/>
+          <Route exact path="/admin/add-site" component = {adminAddSite} type = "adminaddsite"/>
+	  <Route exact path="/admin/view-site" component = {adminViewSite} type = "adminviewsite"/>
+	  <Route exact path="/site" component = {siteOverview} type = "siteView"/>
+	  <Route exact path="/site/case-view" component = {detailView} type = "detailView"/>
+	  <Route exact path="/acc-recovery" component = {accRecovery} type = "accRec"/>
         </Switch>
       </BrowserRouter>
     )
