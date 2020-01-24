225e6309a701c834eecadc89a5afe399d0b367b0
juliahedrick <julia.hedrick3@gmail.com>
2020-01-24 14:34:53 -0500

    removed log statements

--- src/frontend/src/modules/admin/adminViewSite/table.js
+++ src/frontend/src/modules/admin/adminViewSite/table.js
@@ -1,13 +1,9 @@
 import React from "react";
 
 const ObjectTable = (props) => {
-
-  console.log(props.tableRows)
-
   let table = []
   let children = []
   props.tableRows.map(obj => (
-    console.log(obj),
     children = [],
     children.push(<th>{obj[0]}</th>),
     children.push(<td>{obj[1]}</td>),
