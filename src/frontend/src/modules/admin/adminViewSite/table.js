3b402f7277cfaca2bb4d1b125cc7eaa959c4c78e
Nate Hirsch <nate.hirsch@possible.com>
2020-02-05 13:16:26 -0500

    styled fonts/grid

--- src/frontend/src/modules/admin/adminViewSite/table.js
+++ src/frontend/src/modules/admin/adminViewSite/table.js
@@ -1,16 +1,45 @@
 import React from 'react'
+import styled from 'styled-components';
+const  TableHead= styled.th`
+  width: 250px;
+`;
+const  Tablerow2= styled.tr`
+  background-color: rgb(255, 255, 255);
+  font-size: 15px;
+  font-weight: normal;
+  color: rgb(103, 115, 111);
+`;
+const Tablerow1 = styled.tr`
+  background-color: rgb(227, 229, 228);
+  font-size: 15px;
+  font-weight: normal;
+  color: rgb(103, 115, 111);
+`;
 
 
 const ObjectTable = (props) => {
   const table = []
   let children = []
+  let rowNum = 0;
   props.tableRows.map(obj => {
+    rowNum = rowNum+1;
     children = []
-    children.push(<th>{obj[0]}</th>)
+    children.push(<TableHead>{obj[0]}</TableHead>)
     children.push(<td>{obj[1]}</td>)
-    table.push(<thead><tr>{children}</tr></thead>)
+    if(rowNum%2 == 1){
+      table.push(<thead><Tablerow1>{children}</Tablerow1></thead>)
+    }else{
+      table.push(<thead><Tablerow2>{children}</Tablerow2></thead>)
+    }
+
   })
 
-  return (<table>{table}</table>)
+  return (
+  <div>
+    <p>{props.Title}</p>
+    <table>
+      {table}
+    </table>
+  </div>)
 }
 export default ObjectTable
