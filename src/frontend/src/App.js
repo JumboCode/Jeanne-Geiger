0c0989b1bd13078029c18120c761880b9eebad63
Andrew Gross <aleogross@gmail.com>
2020-02-09 15:04:36 -0500

    Added basic frontend login/signup components

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -12,6 +12,10 @@ import { detailView } from './modules/comManager/detailView'
 import { editCase } from './modules/comManager/editCase' */
 import AppRouter from './routes.js'
 
+import Nav from './modules/logIn/Nav';
+import LoginForm from './modules/logIn/LoginForm';
+import SignupForm from './modules/logIn/SignupForm';
+
 /*
 function App () {
   return (
@@ -38,10 +42,115 @@ return <AppRouter />;
 */
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: '',
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: '',
+      token: localStorage.getItem('token')
+    }
+  }
   render () {
     return (
       <AppRouter />
     )
+  }
+
+
+  componentDidMount() {
+    if (this.state.logged_in) {
+      fetch('http://localhost:8000/api/current_user/', {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
+        }
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({ username: json.username });
+        });
+    }
+  }
+
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/token-auth/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.user.username,
+          token: json.token
+        });
+      });
+  };
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch('http://localhost:8000/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.username
+        });
+      });
+  };
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
+  render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case 'signup':
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
+    return (
+      <div className="App">
+        <Nav
+          logged_in={this.state.logged_in}
+          display_form={this.display_form}
+          handle_logout={this.handle_logout}
+        />
+        {form}
+        <h3>
+          {this.state.logged_in
+            ? `Hello, ${this.state.username}! \n Here is your token:\n${this.state.token}`
+            : 'Please Log In'}
+        </h3>
+      </div>
+    );
   }
 }
 
