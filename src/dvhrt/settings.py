8febb8fdd34c7883ab1e5f6aca5bfe23d5e37c3c
Barry Eom <31739208+barieom@users.noreply.github.com>
2020-02-28 15:36:14 -0500

    removed static root in settings.py

--- src/dvhrt/settings.py
+++ src/dvhrt/settings.py
@@ -173,7 +173,7 @@ STATIC_URL = '/static/'
 
 
 # Place static in the same location as webpack build files
-STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'build', 'static')
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static')
