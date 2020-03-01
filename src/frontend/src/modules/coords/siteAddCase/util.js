245381ac545295e1a49be7ba07089f06cf08c9c8
Barry Eom <31739208+barieom@users.noreply.github.com>
2020-03-01 18:32:32 -0500

    eslint fixed

--- src/frontend/src/modules/coords/siteAddCase/util.js
+++ src/frontend/src/modules/coords/siteAddCase/util.js
@@ -2,36 +2,48 @@ import React from 'react'
 
 const DropdownObj = (props) => {
   const elems = []
-  if (props.choices != undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
+  if (props.choices !== undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
 
   return (
-    <p>{props.title}:
-      <select id={props.id} name={props.title} required>
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <select class="form-control" id={props.id} name={props.title} required>
         <option></option>
         {elems}
       </select>
-    </p>
+    </div>
   )
 }
 
 const TextInputObj = (props) => {
-  return (<p>{props.title}: <input type='text' id={props.id} name={props.id}></input></p>)
+  return (
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <input type="text" class="form-control" id={props.id} placeholder={'Enter ' + props.title}></input>
+    </div>
+  )
 }
 
 const DateInputObj = (props) => {
-  return (<p>{props.title}: <input type="date" id={props.id} name={props.id} min="1900-01-01"></input></p>)
+  return (
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <input type="date" class="form-control" id={props.id} placeholder={'Enter ' + props.title}></input>
+    </div>
+  )
 }
 
 const MultSelectionObj = (props) => {
   const elems = []
-  if (props.choices != undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
+  if (props.choices !== undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
 
   return (
-    <p>{props.title}: <br/>
-      <select id={props.id} name={props.title} size={props.size} multiple="multiple" required>
+    <div class="form-group">
+      <label class="mdb-main-label">{props.title}</label>
+      <select class="form-control" id={props.id} name={props.title} size={props.size} multiple required>
         {elems}
       </select>
-    </p>
+    </div>
   )
 }
 
