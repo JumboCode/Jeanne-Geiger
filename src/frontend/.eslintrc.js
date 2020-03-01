92d1eaa219d86b212a381823df4b1643c1f46c96
MahBoiVladdyP <trev0407@gmail.com>
2020-03-01 18:19:53 -0500

    Doing a test of removing package-lock.json

--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: [
     'standard'
   ],
+  parser: "babel-eslint",
+
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
--- src/frontend/src/Comps/Back/back.js
+++ src/frontend/src/Comps/Back/back.js
@@ -5,14 +5,14 @@ import './back.css'
 // This class defines a back button that goes back one page
 // in the user's browser history when called
 export default class BackButton extends React.Component {
-  handleClick = () => {
+  /*handleClick = () => {
     window.history.back()
-  }
+  }*/
 
   render () {
     return (
       <div className="backbutton">
-        <img src={backLogo} alt="Back Button" onClick={this.handleClick}/>
+        <img src={backLogo} alt="Back Button" onClick={window.history.back}/>
       </div>
     )
   }
