1fd0990c8737edba70d4da49de071b1b2c44d87a
trevorMannherz <tmann4242@gmail.com>
2020-04-03 12:49:05 -0400

    initial commit

--- src/frontend/src/modules/admin/adminAddSite/index.js
+++ src/frontend/src/modules/admin/adminAddSite/index.js
@@ -1,15 +1,57 @@
 import React, { Component } from 'react'
 import './styles.css'
 import { render } from 'react-dom'
+import { TextInputObj } from './util.js'
+import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
 import NavigationBar from '../../navbar/NavigationBar.js'
 
+const SITE_POST_URL = 'placeholder'
+
 class adminAddSite extends React.Component {
+  doSitePost() {
+    return 'site_name=' + document.getElementById('site_name').value +
+    '&coord_name=' + document.getElementById('coord_name').value +
+    '&coord_email=' + document.getElementById('coord_email').value +
+    '&referral_1=' + document.getElementById('referral_1').value +
+    '&referral_2=' + document.getElementById('referral_2').value
+  }
+
+  doSubmit () {
+    var siteInfo = this.doSitePost()
+
+    var sitePostRequest = new XMLHttpRequest()
+    sitePostRequest.open('POST', SITE_POST_URL, true)
+    sitePostRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+    sitePostRequest.send(siteInfo)
+    sitePostRequest.onload = function () {
+      return JSON.parse(sitePostRequest.responseText).site_id
+    }
+  }
+
   render () {
     return (
       <div>
         <NavigationBar />
-        <h1>Adding a site</h1>
+        <h2 class="title">Add New Site</h2>
         <h1>{this.props.type}</h1>
+        <div class="container">
+          <Form.Row>
+            <Col>
+              <TextInputObj title='Site Name' id='site_name'/>
+              <TextInputObj title='Coordinator Name' id='coord_name'/>
+              <TextInputObj title='Coordinator Email' id='coord_email'/>
+            </Col>
+            <Col>
+              <TextInputObj title='Referral Source 1' id='referral_1'/>
+              <TextInputObj title='Referral Source 2' id='referral_2'/>
+            </Col>
+          </Form.Row>
+        </div>
+        <div class="buttons">
+          <button class="cancel_button">Cancel</button>
+          <button type="save" class="save_button" onClick={() => this.doSubmit()} value="save">Save</button>
+        </div>
       </div>
     )
   }
---
+++ src/frontend/src/modules/admin/adminAddSite/util.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const DropdownObj = (props) => {
+  const elems = []
+  if (props.choices !== undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
+
+  return (
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <select class="form-control" id={props.id} name={props.title} required>
+        <option></option>
+        {elems}
+      </select>
+    </div>
+  )
+}
+
+const TextInputObj = (props) => {
+  return (
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <input type="text" class="form-control" id={props.id} placeholder={'Enter ' + props.title}></input>
+    </div>
+  )
+}
+
+const DateInputObj = (props) => {
+  return (
+    <div class="form-group">
+      <label for={props.id}>{props.title}</label>
+      <input type="date" class="form-control" id={props.id} placeholder={'Enter ' + props.title}></input>
+    </div>
+  )
+}
+
+const MultSelectionObj = (props) => {
+  const elems = []
+  if (props.choices !== undefined) { props.choices.map(elem => elems.push(<option value={elem[1]}>{elem[0]}</option>)) }
+
+  return (
+    <div class="form-group">
+      <label class="mdb-main-label">{props.title}</label>
+      <select class="form-control" id={props.id} name={props.title} size={props.size} multiple required>
+        {elems}
+      </select>
+    </div>
+  )
+}
+
+export { DropdownObj, TextInputObj, DateInputObj, MultSelectionObj }
