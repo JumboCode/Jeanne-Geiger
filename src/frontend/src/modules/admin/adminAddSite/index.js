e338b2a5771f51f68a8fab6b068315eaef0d2bd7
trevorMannherz <tmann4242@gmail.com>
2020-04-08 12:35:33 -0400

    Referral sources and coords

--- src/frontend/src/modules/admin/adminAddSite/index.js
+++ src/frontend/src/modules/admin/adminAddSite/index.js
@@ -12,9 +12,12 @@ const SITE_POST_URL = 'placeholder'
 
 class adminAddSite extends React.Component {
   state = {
-    sources: []
+    sources: [],
+    coords: []
   }
 
+  // Get both the first referral source and added sources separately
+  // Same with Coords
   doSitePost() {
     return 'site_name=' + document.getElementById('site_name').value +
     '&coord_name=' + document.getElementById('coord_name').value +
@@ -27,9 +30,19 @@ class adminAddSite extends React.Component {
     this.setState({sources: [...this.state.sources, ""]})
   }
 
-  handleRemove(index) {
+  removeSource(index) {
     this.state.sources.splice(index, 1)
     this.setState({sources: this.state.sources})
+  }
+
+  addCoord() {
+    this.setState({coords: [...this.state.coords, ""]})
+  }
+
+  removeCoord(index) {
+    const values = [...]
+    this.state.coords.splice(index, 1)
+    this.setState({coords: this.state.coords})
   }
 
   render () {
@@ -42,24 +55,41 @@ class adminAddSite extends React.Component {
           <Form.Row>
             <Col>
               <TextInputObj title='Site Name' id='site_name'/>
-              <TextInputObj title='Coordinator Name' id='coord_name'/>
-              <TextInputObj title='Coordinator Email' id='coord_email'/>
-            </Col>
-            <Col>
+              <TextInputObj title='Coordinator 1 Name' id='coord_name_1'/>
+              <TextInputObj title='Coordinator 1 Email' id='coord_email_1'/>
               {
-                this.state.sources.map((source, i) => {
+                this.state.coords.map((coords, i) => {
                   return (
                     <div key={i}>
-                      <TextInputObj class="referral" title={'Referral Source ' + (i + 1)} id={'referral_' + (i + 1)} />
+                      <TextInputObj title={'Coordinator ' + (i + 2) + ' Name'} id={'coord_name_' + (i + 2)} />
+                      <TextInputObj title={'Coordinator ' + (i + 2) + ' Email'} id={'coord_email_' + (i + 2)} />
                       <div class="buttons">
-                        <button class="removeSource" onClick={(e)=>this.handleRemove(i)}><img class="logo" src={Remove} /></button>
+                        <button class="remove" onClick={(e)=>this.removeCoord(i)}><img class="logo" src={Remove} /></button>
                       </div>
                     </div>
                   )
                 })
               }
               <div class="buttons">
-                <button class="addSource" onClick={(e)=>this.addSource(e)}><img class="logo" src={Plus} /> Add another Referral Source</button>
+                <button class="add" onClick={(e)=>this.addCoord(e)}><img class="logo" src={Plus} /> Add another Coordinator</button>
+              </div>
+            </Col>
+            <Col>
+              <TextInputObj class="referral" title='Referral Source 1' id='referral_1' />
+              {
+                this.state.sources.map((source, i) => {
+                  return (
+                    <div key={i}>
+                      <TextInputObj class="referral" title={'Referral Source ' + (i + 2)} id={'referral_' + (i + 1)} />
+                      <div class="buttons">
+                        <button class="remove" onClick={(e)=>this.removeSource(i)}><img class="logo" src={Remove} /></button>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+              <div class="buttons">
+                <button class="add" onClick={(e)=>this.addSource(e)}><img class="logo" src={Plus} /> Add another Referral Source</button>
               </div>
 
             </Col>
