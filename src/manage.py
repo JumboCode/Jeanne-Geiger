175c0dfcb4c344cbb2378e8f31c92138d3dd3f06
Andrew Gross <aleogross@gmail.com>
2020-05-08 13:28:56 -0400

    Attempt one at setting up production branch

--- src/manage.py
+++ src/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvhrt.settings_prod')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dvhrt.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
