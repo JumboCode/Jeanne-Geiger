5a68ccd7cdceb8b098f5b401b2a036836eea0055
Nicole <nlkennedy7@gmail.com>
2020-05-12 11:33:28 -0400

    redirect after login

--- src/frontend/src/modules/logIn/index.js
+++ src/frontend/src/modules/logIn/index.js
@@ -10,47 +10,43 @@ import { Redirect } from 'react-router-dom'
 const Login = () => {
   const { roles, isAuthenticated, loginWithRedirect, logout } = useAuth0()
 
-  return (
-    <div className='auth0button'>
-      {!isAuthenticated && (
-        <button onClick={() => loginWithRedirect({})}>Log in Login</button>
-      )}
+//   return (
+//     <div className='auth0button'>
+//       {!isAuthenticated && (
+//         <button onClick={() => loginWithRedirect({})}>Log in Login</button>
+//       )}
 
-      {isAuthenticated && roles.includes('DVHRT_ADMIN') ? <Redirect to='/admin' /> : <Redirect to='/site' />}
-    </div>
-  )
-    // return (
-    //   <div className="login_container">
-    //     <div className="item_container">
-    //       <div className="logo_container">
-    //         <Logo className="logo_style"/>
-    //         <p1 className="dom_text">
-    //                 Domestic Violence High-Risk Team Login
-    //         </p1>
-    //       </div>
-    //     </div>
-    //     <div class="vl"></div>
-    //     <div className="item_container">
-    //       <div className="text_container">
-    //         <form>
-    //           <input type="email" className="email_form" placeholder="  Email"/>
-    //           <input type="password" className="password_form" placeholder="  Password"/>
-    //         </form>
-    //         <p1 className="forgot_text">
-    //                 Forgot your username or password?
-    //         </p1>
-    //       </div>
-    //       <div className="button_container">
-    //         <a href="/admin"><button className="admin_button" onclick="location.href='/admin'">
-    //                 Admin
-    //         </button></a>
-    //         <a href="/site"><button className="coord_button">
-    //                 Coordinator
-    //         </button></a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // )
+      
+//     </div>
+//   )
+    return (
+      <div className="login_container">
+        <div className="item_container">
+          <div className="logo_container">
+            <Logo className="logo_style"/>
+            <p1 className="dom_text">
+                    Domestic Violence High-Risk Team Login
+            </p1>
+          </div>
+        </div>
+        <div class="vl"></div>
+        <div className="item_container">
+          <div className="text_container">
+            <form>
+              <input type="email" className="email_form" placeholder="  Email"/>
+              <input type="password" className="password_form" placeholder="  Password"/>
+            </form>
+            <p1 className="forgot_text">
+                    Forgot your username or password?
+            </p1>
+          </div>
+          <div className="button_container">
+            <button className="coord_button" onClick={() => loginWithRedirect({redirect_uri: 'http://localhost:3000/site/'})}>Coordinator</button>
+            <button className="admin_button" onClick={() => loginWithRedirect({redirect_uri: 'http://localhost:3000/admin/'})}>Admin</button>
+          </div>
+        </div>
+      </div>
+    )
 }
 
 export default Login
