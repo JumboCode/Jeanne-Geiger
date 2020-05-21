e445b280c483a9d1603d23d1fd56ecb5fa9419b4
Barry Eom <31739208+barieom@users.noreply.github.com>
2020-05-21 13:19:32 -0400

    force https/SSL

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -7,6 +7,11 @@ import { Auth0Provider } from './react-auth0-spa'
 import config from './auth_config.json'
 import history from './utils/history'
 
+
+var sslRedirect = require(‘heroku-ssl-redirect’);
+const app = express();
+app.use(sslRedirect());
+
 // A function that routes the user to the right place
 // after login
 const onRedirectCallback = appState => {
