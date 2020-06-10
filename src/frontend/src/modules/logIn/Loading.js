5eded7dfa2b9f07c3a26bff0361bd069fa03c91b
trevorMannherz <tmann4242@gmail.com>
2020-06-09 20:43:56 -0400

    fix eslint

--- src/frontend/src/modules/logIn/Loading.js
+++ src/frontend/src/modules/logIn/Loading.js
@@ -1,33 +1,33 @@
 import React from 'react'
-import { css } from "@emotion/core";
-import FadeLoader from "react-spinners/FadeLoader";
- 
+import { css } from '@emotion/core'
+import FadeLoader from 'react-spinners/FadeLoader'
+
 // Can be a string as well. Need to ensure each key-value pair ends with ;
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: red;
-`;
- 
+`
+
 class Loading extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       loading: true
-    };
+    }
   }
- 
-  render() {
+
+  render () {
     return (
       <div className="loading">
         <FadeLoader
           css={override}
           size={40}
           margin={2}
-          color={"#73296e"}
+          color={'#73296e'}
           loading={this.state.loading}
         />
       </div>
-    );
+    )
   }
 } export default Loading
